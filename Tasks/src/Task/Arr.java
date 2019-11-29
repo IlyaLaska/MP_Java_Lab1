65c83a1e2dc96030ffd794ea1bf217c041bfb1d4
IlyaLaska <IlyaLaska@users.noreply.github.com>
2019-11-29 12:13:29 +0200

    Added task 5

--- Tasks/src/Task/Arr.java
+++ Tasks/src/Task/Arr.java
@@ -1,7 +1,6 @@
 package Task;
 
-import java.util.Arrays;
-import java.util.NoSuchElementException;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Arr
@@ -38,22 +37,51 @@ public class Arr
         IntStream stream = Arrays.stream(array);
         return stream.filter(val -> val > 0).count();
     }
-    public IntStream mul(int num) {
+    public int[] mul(int num) {
         IntStream stream = Arrays.stream(array);
-        return stream.map(el -> el * num);
+        return stream.map(el -> el * num).toArray();
     }
-    public IntStream indices(int toMatch) {
+    public int[] indices(int toMatch) {
         IntStream stream1 = Arrays.stream(array);
         int[] toArr = stream1.toArray();
         IntStream stream = Arrays.stream(array);
         IntStream res = IntStream.range(0, (int) stream.count()).filter(pos -> toArr[pos] == toMatch);
-        return res;
+        return res.toArray();
     }
-    public IntStream nonDistinct() {
-        IntStream stream1 = Arrays.stream(array);
-        IntStream stream2 = Arrays.stream(array);
-        IntStream distinct = stream1.distinct();
-
-        return distinct;
+    public Object[] t2() {
+        int[] arr2 = Arrays.stream(array).sorted().toArray();
+        Vector<Integer> dupl = new Vector<Integer>(3, 3);
+        IntStream.range(1, (int) arr2.length).forEach(pos -> {
+            if(arr2[pos] == arr2[pos-1]) {
+                dupl.addElement(arr2[pos]);
+            }
+        });
+        return dupl.toArray();
     }
+//    public int[] nonDup() {
+//        IntStream stream1 = Arrays.stream(array).sorted();
+//    }
+//    public int[] t() {
+//        IntStream stream1 = Arrays.stream(array).sorted();
+//        int[] res = stream1.reduce(new Pair(0, new LinkedList<Integer>()), el -> {
+//
+//        })
+//        return res;
+//    }
+//    public int[] nonDistinct() {
+//        IntStream stream1 = Arrays.stream(array);
+//        IntStream stream2 = Arrays.stream(array);
+//        IntStream stream3 = Arrays.stream(array);
+//        IntStream distinct = stream1.distinct();
+//        int[] res = IntStream.range(0, (int) stream2.count()).filter(pos -> {
+//
+//        }) stream3.filter(el -> el == ).toArray();
+////        int[] res = stream2.reduce(new ArrayList(), (arr, el) -> {
+////            IntStream newStr = Arrays.stream(array);
+//////            if()
+////            return arr.add();
+////        });
+////        res = stream2.filter(el -> el.anyMatch)
+//        return res;
+//    }
 }
--- Tasks/src/Task/Main.java
+++ Tasks/src/Task/Main.java
@@ -1,13 +1,14 @@
 package Task;
 
 //import java.util.Arrays;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 //import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to JAVA Lab1!");
-        Arr a = new Arr(new int[]{1, 2, 3, 4, 1});
+        Arr a = new Arr(new int[]{1, 2, 3, 4, 1, 2});
 //        int[] b = {1, 2, 3, 4, 5};
 //        System.out.println(Arrays.toString(a.getArray()));
         System.out.println("Task 1");
@@ -15,12 +16,12 @@ public class Main {
         System.out.println("Task 2");
         System.out.println(a.count());
         System.out.println("Task 3");
-        IntStream mulRes = a.mul(5);
-        System.out.println(mulRes);
+        int[] mulRes = a.mul(5);
+        System.out.println(Arrays.toString(mulRes));
         System.out.println("Task 4");
-        System.out.println(a.indices(1));
-//        System.out.println("Task 5");
-//        System.out.println(a.nonDistinct());
+        System.out.println(Arrays.toString(a.indices(1)));
+        System.out.println("Task 5");
+        System.out.println(Arrays.toString(a.t2()));
     }
 }
 //5 6 15 17 2
